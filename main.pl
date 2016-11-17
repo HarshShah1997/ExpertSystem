39e69f6801a61472ab170b065e5aa2f2ca56b229
Harsh Shah <harsh.shah.jayesh@gmail.com>
2016-11-17 20:32:29 +0530

    Added description

--- main.pl
+++ main.pl
@@ -1,17 +1,17 @@
 % Expert system should be started from here
 main :-
-  welcome,
-  findLanguage(Language),
-  describe(Language), nl.
+    welcome,
+    findLanguage(Language),
+    describe(Language), nl.
 
 
 welcome :-
-  write('Which programming language should I learn first in 2016?'), nl,
-  write('To answer, input the number shown next to each answer, followed by a dot (.)'), nl, nl.
+    write('Which programming language should I learn first in 2016?'), nl,
+    write('To answer, input the number shown next to each answer, followed by a dot (.)'), nl, nl.
 
 
 findLanguage(Language) :-
-  language(Language), !.
+    language(Language), !.
 
 
 % Store user answers to be able to track his progress
@@ -19,457 +19,466 @@ findLanguage(Language) :-
 
 % Rules for the knowledge base
 language(python) :-
-  why(forChildren).
+    why(forChildren).
 
 language(python) :-
-  why(donotKnow).
+    why(donotKnow).
 
 language(java) :-
-  why(earnMoney),
-  prefferedPlatform(doesNotMatter).
+    why(earnMoney),
+    prefferedPlatform(doesNotMatter).
 
 language(cpp) :-
-  why(earnMoney),
-  prefferedPlatform(gaming).
+    why(earnMoney),
+    prefferedPlatform(gaming).
 
 language(objectivec) :-
-  why(earnMoney),
-  prefferedPlatform(mobile),
-  preferredMobileOS(ios).
+    why(earnMoney),
+    prefferedPlatform(mobile),
+    preferredMobileOS(ios).
 
 language(java) :-
-  why(earnMoney),
-  prefferedPlatform(mobile),
-  preferredMobileOS(android).
+    why(earnMoney),
+    prefferedPlatform(mobile),
+    preferredMobileOS(android).
 
 language(python) :-
-  why(earnMoney),
-  prefferedPlatform(facebook).
+    why(earnMoney),
+    prefferedPlatform(facebook).
 
 language(python) :-
-  why(earnMoney),
-  prefferedPlatform(google).
+    why(earnMoney),
+    prefferedPlatform(google).
 
 language(csharp) :-
-  why(earnMoney),
-  prefferedPlatform(microsoft).
+    why(earnMoney),
+    prefferedPlatform(microsoft).
 
 language(objectivec) :-
-  why(earnMoney),
-  prefferedPlatform(apple).
+    why(earnMoney),
+    prefferedPlatform(apple).
 
 language(javascript) :-
-  why(earnMoney),
-  prefferedPlatform(web),
-  web(frontEnd).
+    why(earnMoney),
+    prefferedPlatform(web),
+    web(frontEnd).
 
 language(csharp) :-
-  why(earnMoney),
-  prefferedPlatform(web),
-  web(backEnd),
-  workFor(corporate),
-  microsoftOpinion(iAmFan).
+    why(earnMoney),
+    prefferedPlatform(web),
+    web(backEnd),
+    workFor(corporate),
+    microsoftOpinion(iAmFan).
 
 language(java) :-
-  why(earnMoney),
-  prefferedPlatform(web),
-  web(backEnd),
-  workFor(corporate),
-  microsoftOpinion(goodEnough).
+    why(earnMoney),
+    prefferedPlatform(web),
+    web(backEnd),
+    workFor(corporate),
+    microsoftOpinion(goodEnough).
 
 language(java) :-
-  why(earnMoney),
-  prefferedPlatform(web),
-  web(backEnd),
-  workFor(corporate),
-  microsoftOpinion(suck).
+    why(earnMoney),
+    prefferedPlatform(web),
+    web(backEnd),
+    workFor(corporate),
+    microsoftOpinion(suck).
 
 language(javascript) :-
-  why(earnMoney),
-  prefferedPlatform(web),
-  web(backEnd),
-  workFor(startup),
-  tryNew(yes).
+    why(earnMoney),
+    prefferedPlatform(web),
+    web(backEnd),
+    workFor(startup),
+    tryNew(yes).
 
 language(python) :-
-  why(earnMoney),
-  prefferedPlatform(web),
-  web(backEnd),
-  workFor(startup),
-  tryNew(no),
-  favouriteToy(lego).
+    why(earnMoney),
+    prefferedPlatform(web),
+    web(backEnd),
+    workFor(startup),
+    tryNew(no),
+    favouriteToy(lego).
 
 language(ruby) :-
-  why(earnMoney),
-  prefferedPlatform(web),
-  web(backEnd),
-  workFor(startup),
-  tryNew(no),
-  favouriteToy(playDoh).
+    why(earnMoney),
+    prefferedPlatform(web),
+    web(backEnd),
+    workFor(startup),
+    tryNew(no),
+    favouriteToy(playDoh).
 
 language(php) :-
-  why(earnMoney),
-  prefferedPlatform(web),
-  web(backEnd),
-  workFor(startup),
-  tryNew(no),
-  favouriteToy(oldUgly).
+    why(earnMoney),
+    prefferedPlatform(web),
+    web(backEnd),
+    workFor(startup),
+    tryNew(no),
+    favouriteToy(oldUgly).
 
 language(csharp) :-
-  why(earnMoney),
-  prefferedPlatform(enterprise),
-  microsoftOpinion(iAmFan).
+    why(earnMoney),
+    prefferedPlatform(enterprise),
+    microsoftOpinion(iAmFan).
 
 language(java) :-
-  why(earnMoney),
-  workFor(enterprise),
-  microsoftOpinion(goodEnough).
+    why(earnMoney),
+    workFor(enterprise),
+    microsoftOpinion(goodEnough).
 
 language(java) :-
-  why(earnMoney),
-  workFor(enterprise),
-  microsoftOpinion(suck).
+    why(earnMoney),
+    workFor(enterprise),
+    microsoftOpinion(suck).
 
 language(python) :-
-  why(forFun),
-  learningPreference(easyWay).
+    why(forFun),
+    learningPreference(easyWay).
 
 language(python) :-
-  why(forFun),
-  learningPreference(bestWay).
+    why(forFun),
+    learningPreference(bestWay).
 
 language(java) :-
-  why(forFun),
-  learningPreference(harderWay),
-  whichCar(auto).
+    why(forFun),
+    learningPreference(harderWay),
+    whichCar(auto).
 
 language(c) :-
-  why(forFun),
-  learningPreference(harderWay),
-  whichCar(manual).
+    why(forFun),
+    learningPreference(harderWay),
+    whichCar(manual).
 
 language(cpp) :-
-  why(forFun),
-  learningPreference(hardestWay).
+    why(forFun),
+    learningPreference(hardestWay).
 
 language(python) :-
-  why(forInterest),
-  learningPreference(easyWay).
+    why(forInterest),
+    learningPreference(easyWay).
 
 language(python) :-
-  why(forInterest),
-  learningPreference(bestWay).
+    why(forInterest),
+    learningPreference(bestWay).
 
 language(java) :-
-  why(forInterest),
-  learningPreference(harderWay),
-  whichCar(auto).
+    why(forInterest),
+    learningPreference(harderWay),
+    whichCar(auto).
 
 language(c) :-
-  why(forInterest),
-  learningPreference(harderWay),
-  whichCar(manual).
+    why(forInterest),
+    learningPreference(harderWay),
+    whichCar(manual).
 
 language(cpp) :-
-  why(forInterest),
-  learningPreference(hardestWay).
+    why(forInterest),
+    learningPreference(hardestWay).
 
 language(python) :-
-  why(toImprove),
-  learningPreference(easyWay).
+    why(toImprove),
+    learningPreference(easyWay).
 
 language(python) :-
-  why(toImprove),
-  learningPreference(bestWay).
+    why(toImprove),
+    learningPreference(bestWay).
 
 language(java) :-
-  why(toImprove),
-  learningPreference(harderWay),
-  whichCar(auto).
+    why(toImprove),
+    learningPreference(harderWay),
+    whichCar(auto).
 
 language(c) :-
-  why(toImprove),
-  learningPreference(harderWay),
-  whichCar(manual).
+    why(toImprove),
+    learningPreference(harderWay),
+    whichCar(manual).
 
 language(cpp) :-
-  why(toImprove),
-  learningPreference(hardestWay).
+    why(toImprove),
+    learningPreference(hardestWay).
 
 
 % Questions for the knowledge base
 question(why) :-
-  write('Why do you want to learn programming?'), nl.
+    write('Why do you want to learn programming?'), nl.
 
 question(prefferedPlatform) :-
-  write('Which platform/field?'), nl.
+    write('Which platform/field?'), nl.
 
 question(preferredMobileOS) :-
-  write('Which OS?'), nl.
+    write('Which OS?'), nl.
 
 question(web) :-
-  write('Which "end"?'), nl.
+    write('Which "end"?'), nl.
 
 question(workFor) :-
-  write('I want to work for...'), nl.
+    write('I want to work for...'), nl.
 
 question(microsoftOpinion) :-
-  write('What do you think about Microsoft?'), nl.
+    write('What do you think about Microsoft?'), nl.
 
 question(tryNew) :-
-  write('Do you want to try something new, with huge potential, but less mature?'), nl.
+    write('Do you want to try something new, with huge potential, but less mature?'), nl.
 
 question(favouriteToy) :-
-  write('Which one is your favourite toy?'), nl.
+    write('Which one is your favourite toy?'), nl.
 
 question(learningPreference) :-
-  write('I prefer to learn things...'), nl.
+    write('I prefer to learn things...'), nl.
 
 question(whichCar) :-
-  write('Auto or Manual Car?'), nl.
+    write('Auto or Manual Car?'), nl.
 
 
 % Answers for the knowledge base
 answer(forChildren) :-
-  write('For my kids').
+    write('For my kids').
 
 answer(donotKnow) :-
-  write('I don\'t know').
+    write('I don\'t know').
 
 answer(earnMoney) :-
-  write('Make money').
+    write('Make money').
 
 answer(forFun) :-
-  write('Just for fun').
+    write('Just for fun').
 
 answer(forInterest) :-
-  write('I\'m interested').
+    write('I\'m interested').
 
 answer(toImprove) :-
-  write('Improve myself').
+    write('Improve myself').
 
 answer(doesNotMatter) :-
-  write('Doesn\'t matter, I just want $$$').
+    write('Doesn\'t matter, I just want $$$').
 
 answer(gaming) :-
-  write('3D/Gaming').
+    write('3D/Gaming').
 
 answer(mobile) :-
-  write('Mobile').
+    write('Mobile').
 
 answer(facebook) :-
-  write('Facebook').
+    write('Facebook').
 
 answer(google) :-
-  write('Google').
+    write('Google').
 
 answer(microsoft) :-
-  write('Microsoft').
+    write('Microsoft').
 
 answer(apple) :-
-  write('Apple').
+    write('Apple').
 
 answer(web) :-
-  write('Web').
+    write('Web').
 
 answer(enterprise) :-
-  write('Enterprise').
+    write('Enterprise').
 
 answer(ios) :-
-  write('iOS').
+    write('iOS').
 
 answer(android) :-
-  write('Android').
+    write('Android').
 
 answer(frontEnd) :-
-  write('Front-end').
+    write('Front-end').
 
 answer(backEnd) :-
-  write('Back-end').
+    write('Back-end').
 
 answer(startup) :-
-  write('Startup').
+    write('Startup').
 
 answer(corporate) :-
-  write('Corporate').
+    write('Corporate').
 
 answer(iAmFan) :-
-  write('I\'m a fan!').
+    write('I\'m a fan!').
 
 answer(goodEnough) :-
-  write('Not Bad').
+    write('Not Bad').
 
 answer(suck) :-
-  write('Suck').
+    write('Suck').
 
 answer(yes) :-
-  write('Yes').
+    write('Yes').
 
 answer(no) :-
-  write('No').
+    write('No').
 
 answer(lego) :-
-  write('Lego').
+    write('Lego').
 
 answer(playDoh) :-
-  write('Play-Doh').
+    write('Play-Doh').
 
 answer(oldUgly) :-
-  write('I\'ve an old & ugly toy, but I love it so much!').
+    write('I\'ve an old & ugly toy, but I love it so much!').
 
 answer(easyWay) :-
-  write('The easy way').
+    write('The easy way').
 
 answer(bestWay) :-
-  write('The best way').
+    write('The best way').
 
 answer(harderWay) :-
-  write('The slightly harder way').
+    write('The slightly harder way').
 
 answer(hardestWay) :-
-  write('The really hard way (but easier to pick up other languages in the future)').
+    write('The really hard way (but easier to pick up other languages in the future)').
 
 answer(auto) :-
-  write('Auto').
+    write('Auto').
 
 answer(manual) :-
-  write('Manual').
+    write('Manual').
 
 
 % Language descriptions for the knowledge base
 describe(python) :-
-  write('Python'), nl,
-  write('Widely regarded as the best programming language for beginners'), nl,
-  write('Easiest to learn').
+    write('Python'), nl,
+    write('Widely regarded as the best programming language for beginners'), nl,
+    write('Easiest to learn'), nl, 
+    write('You can Build website using Django a popular framework for python') ,nl ,
+    write('Widely Used in scientific,technical and academic Field such as Artificial Intelligence ') , nl.
 
 describe(java) :-
-  write('Java'), nl,
-  write('One of the most in demand & highest paying programming languages'), nl,
-  write('Slogan: write once, work everywhere').
+    write('Java'), nl,
+    write('One of the most in demand & highest paying programming languages'), nl,
+    write('Slogan: write once, work everywhere') , nl ,
+    write('Very popular on all platforms because of its portability ') , nl.
 
 describe(c) :-
-  write('C'), nl,
-  write('Lingua franca of programming language'), nl,
-  write('One of the oldest and most widely used language in the world').
+    write('C'), nl,
+    write('Lingua franca of programming language'), nl,
+    write('One of the oldest and most widely used language in the world'), nl ,
+    write('Popular language for System and Hardware programming ') , nl.
 
 describe(cpp) :-
-  write('C++'), nl,
-  write('Complex version of C with a lot more features'), nl,
-  write('Recommended only if you have a mentor to guide you').
+    write('C++'), nl,
+    write('Complex version of C with a lot more features'), nl,
+    write('Widely used for developing games , industrial and performance critical applications'), nl,
+    write('Recommended only if you have a mentor to guide you').
 
 describe(javascript) :-
-  write('JavaScript'), nl,
-  write('Most popular clients-side web scripting language'), nl,
-  write('A must learn for front-end web developer (HTML and CSS as well)').
+    write('JavaScript'), nl,
+    write('Most popular clients-side web scripting language'), nl,
+    write('One of the hottest programming language now, due to its increasing popularity as server side language(node.js)'), nl,
+    write('A must learn for front-end web developer (HTML and CSS as well)').
 
 describe(csharp) :-
-  write('C#'), nl,
-  write('A popular choice for enterprise to create websites and Windows application using .NET framework'), nl,
-  write('Similar to Java in basic syntax and some features').
+    write('C#'), nl,
+    write('A popular choice for enterprise to create websites and Windows application using .NET framework'), nl,
+    write('Can be used to build websites with ASP.net a web Framework from Microsoft'), nl,
+    write('Similar to Java in basic syntax and some features').
 
 describe(ruby) :-
-  write('Ruby'), nl,
-  write('Mostly known for its popular web framework, Ruby on Rails'), nl,
-  write('Focuses on getting things done').
+    write('Ruby'), nl,
+    write('Mostly known for its popular web framework, Ruby on Rails'), nl,
+    write('Designed for fun and productive coding'), nl,
+    write('Focuses on getting things done').
 
 describe(php) :-
-  write('PHP'), nl,
-  write('Suitable for building small and simple sites within a short time frame'), nl,
-  write('Supported by almost every web hosting services with lower price').
+    write('PHP'), nl,
+    write('Suitable for building small and simple sites within a short time frame'), nl,
+    write('Supported by almost every web hosting services with lower price').
 
 describe(objectivec) :-
-  write('Objective-C'), nl,
-  write('Primary language used by Apple for MacOSX & iOS'), nl,
-  write('Choose this if you want to focus on developing iOS or OSX apps only').
+    write('Objective-C'), nl,
+    write('Primary language used by Apple for MacOSX & iOS'), nl,
+    write('Choose this if you want to focus on developing iOS or OSX apps only').
+
 
 
 % Assigns an answer to questions from the knowledge base
 why(Answer) :-
-  progress(why, Answer).
+    progress(why, Answer).
 why(Answer) :-
-  \+ progress(why, _),
-  ask(why, Answer, [forChildren, donotKnow, earnMoney, forFun, forInterest, toImprove]).
+    \+ progress(why, _),
+    ask(why, Answer, [forChildren, donotKnow, earnMoney, forFun, forInterest, toImprove]).
 
 prefferedPlatform(Answer) :-
-  progress(prefferedPlatform, Answer).
+    progress(prefferedPlatform, Answer).
 prefferedPlatform(Answer) :-
-  \+ progress(prefferedPlatform, _),
-  ask(prefferedPlatform, Answer, [doesNotMatter, gaming, mobile, facebook, google, microsoft, apple, web, enterprise]).
+    \+ progress(prefferedPlatform, _),
+    ask(prefferedPlatform, Answer, [doesNotMatter, gaming, mobile, facebook, google, microsoft, apple, web, enterprise]).
 
 preferredMobileOS(Answer) :-
-  progress(preferredMobileOS, Answer).
+    progress(preferredMobileOS, Answer).
 preferredMobileOS(Answer) :-
-  \+ progress(preferredMobileOS, _),
-  ask(preferredMobileOS, Answer, [ios, android]).
+    \+ progress(preferredMobileOS, _),
+    ask(preferredMobileOS, Answer, [ios, android]).
 
 web(Answer) :-
-  progress(web, Answer).
+    progress(web, Answer).
 web(Answer) :-
-  \+ progress(web, _),
-  ask(web, Answer, [frontEnd, backEnd]).
+    \+ progress(web, _),
+    ask(web, Answer, [frontEnd, backEnd]).
 
 workFor(Answer) :-
-  progress(workFor, Answer).
+    progress(workFor, Answer).
 workFor(Answer) :-
-  \+ progress(workFor, _),
-  ask(workFor, Answer, [startup, corporate]).
+    \+ progress(workFor, _),
+    ask(workFor, Answer, [startup, corporate]).
 
 microsoftOpinion(Answer) :-
-  progress(microsoftOpinion, Answer).
+    progress(microsoftOpinion, Answer).
 microsoftOpinion(Answer) :-
-  \+ progress(microsoftOpinion, _),
-  ask(microsoftOpinion, Answer, [iAmFan, goodEnough, suck]).
+    \+ progress(microsoftOpinion, _),
+    ask(microsoftOpinion, Answer, [iAmFan, goodEnough, suck]).
 
 tryNew(Answer) :-
-  progress(tryNew, Answer).
+    progress(tryNew, Answer).
 tryNew(Answer) :-
-  \+ progress(tryNew, _),
-  ask(tryNew, Answer, [yes, no]).
+    \+ progress(tryNew, _),
+    ask(tryNew, Answer, [yes, no]).
 
 favouriteToy(Answer) :-
-  progress(favouriteToy, Answer).
+    progress(favouriteToy, Answer).
 favouriteToy(Answer) :-
-  \+ progress(favouriteToy, _),
-  ask(favouriteToy, Answer, [lego, playDoh, oldUgly]).
+    \+ progress(favouriteToy, _),
+    ask(favouriteToy, Answer, [lego, playDoh, oldUgly]).
 
 learningPreference(Answer) :-
-  progress(learningPreference, Answer).
+    progress(learningPreference, Answer).
 learningPreference(Answer) :-
-  \+ progress(learningPreference, _),
-  ask(learningPreference, Answer, [easyWay, bestWay, harderWay, hardestWay]).
+    \+ progress(learningPreference, _),
+    ask(learningPreference, Answer, [easyWay, bestWay, harderWay, hardestWay]).
 
 whichCar(Answer) :-
-  progress(whichCar, Answer).
+    progress(whichCar, Answer).
 whichCar(Answer) :-
-  \+ progress(whichCar, _),
-  ask(whichCar, Answer, [auto, manual]).
+    \+ progress(whichCar, _),
+    ask(whichCar, Answer, [auto, manual]).
 
 
 % Outputs a nicely formatted list of answers
 % [First|Rest] is the Choices list, Index is the index of First in Choices
 answers([], _).
 answers([First|Rest], Index) :-
-  write(Index), write(' -> '), answer(First), nl,
-  NextIndex is Index + 1,
-  answers(Rest, NextIndex).
+    write(Index), write(' -> '), answer(First), nl,
+    NextIndex is Index + 1,
+    answers(Rest, NextIndex).
 
 
 % Parses an Index and returns a Response representing the "Indexth" element in
 % Choices (the [First|Rest] list)
 parse(0, [First|_], First).
 parse(Index, [First|Rest], Response) :-
-  Index > 0,
-  NextIndex is Index - 1,
-  parse(NextIndex, Rest, Response).
+    Index > 0,
+    NextIndex is Index - 1,
+    parse(NextIndex, Rest, Response).
 
 
 % Asks the Question to the user and saves the Answer
 ask(Question, Answer, Choices) :-
-  question(Question),
-  answers(Choices, 0),
-  read(Index),
-  write(''), nl,
-  parse(Index, Choices, Response),
-  asserta(progress(Question, Response)),
-  Response = Answer.
+    question(Question),
+    answers(Choices, 0),
+    read(Index),
+    write(''), nl,
+    parse(Index, Choices, Response),
+    asserta(progress(Question, Response)),
+    Response = Answer.
 
