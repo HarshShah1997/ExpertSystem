212a656f65b7c6e38831fc7f2247a4ab8b52181c
Harsh Shah <harsh.shah.jayesh@gmail.com>
2016-11-18 23:04:28 +0530

    Nothing

--- languageRecommender.pl
+++ languageRecommender.pl
@@ -1,23 +1,21 @@
-% Expert system should be started from here
-main :-
-    welcome,
-    findLanguage(Language),
-    describe(Language), nl.
+getString(0, [First|_], First).
+getString(Index, [First|Rest], Response) :-
+    Index > 0,
+    NextIndex is Index - 1,
+    getString(NextIndex, Rest, Response).
 
-
-welcome :-
-    write('Which programming language should I learn first in 2016?'), nl,
-    write('To answer, input the number shown next to each answer, followed by a dot (.)'), nl, nl.
-
+askAQuestion(Question, Answer, Choices) :-
+    question(Question),
+    answers(Choices, 0),
+    read(Index),
+    write(''), nl,
+    getString(Index, Choices, Response),
+    asserta(progress(Question, Response)),
+    Response = Answer.
 
 findLanguage(Language) :-
     language(Language), !.
 
-
-% Store user answers to be able to track his progress
-:- dynamic(progress/2).
-
-% Rules for the knowledge base
 language(python) :-
     why(forChildren).
 
@@ -41,6 +39,8 @@ language(java) :-
     why(earnMoney),
     prefferedPlatform(mobile),
     preferredMobileOS(android).
+
+:- dynamic(progress/2).
 
 language(python) :-
     why(earnMoney),
@@ -196,31 +196,16 @@ language(cpp) :-
     why(toImprove),
     learningPreference(hardestWay).
 
+welcome :-
+    write('Which programming language should I learn first in 2016?'), nl,
+    write('To answer, input the number shown next to each answer, followed by a dot (.)'), nl, nl.
 
-% Questions for the knowledge base
-question(why) :-
-    write('Why do you want to learn programming?'), nl.
-
-question(prefferedPlatform) :-
-    write('Which platform/field?'), nl.
-
-question(preferredMobileOS) :-
-    write('Which OS?'), nl.
 
 question(web) :-
     write('Which "end"?'), nl.
 
 question(workFor) :-
     write('I want to work for...'), nl.
-
-question(microsoftOpinion) :-
-    write('What do you think about Microsoft?'), nl.
-
-question(tryNew) :-
-    write('Do you want to try something new, with huge potential, but less mature?'), nl.
-
-question(favouriteToy) :-
-    write('Which one is your favourite toy?'), nl.
 
 question(learningPreference) :-
     write('I prefer to learn things...'), nl.
@@ -229,45 +214,54 @@ question(whichCar) :-
     write('Auto or Manual Car?'), nl.
 
 
-% Answers for the knowledge base
 answer(forChildren) :-
-    write('For my kids').
+    write('I want to learn programming for my children').
 
 answer(donotKnow) :-
-    write('I don\'t know').
+    write('I have no idea').
 
 answer(earnMoney) :-
-    write('Make money').
+    write('I just want to earn money').
+
+question(why) :-
+    write('What is your motivation to learn programming?'), nl.
+
+question(prefferedPlatform) :-
+    write('Which platform would you prefer?'), nl.
+
+question(preferredMobileOS) :-
+    write('Which Operating System would you like?'), nl.
+
 
 answer(forFun) :-
-    write('Just for fun').
+    write('Just for having fun').
 
 answer(forInterest) :-
-    write('I\'m interested').
+    write('I am interested to learn programming').
 
 answer(toImprove) :-
-    write('Improve myself').
+    write('I want to improve myself').
 
 answer(doesNotMatter) :-
-    write('Doesn\'t matter, I just want $$$').
+    write('Does not matter, I just want money').
+
+question(microsoftOpinion) :-
+    write('What is your opinion about Microsoft?'), nl.
+
+question(tryNew) :-
+    write('Do you want to try something new, which has high potential, but is not much mature?'), nl.
+
+question(favouriteToy) :-
+    write('What is your favourite toy?'), nl.
 
 answer(gaming) :-
-    write('3D/Gaming').
+    write('Gaming').
 
 answer(mobile) :-
     write('Mobile').
 
 answer(facebook) :-
     write('Facebook').
-
-answer(google) :-
-    write('Google').
-
-answer(microsoft) :-
-    write('Microsoft').
-
-answer(apple) :-
-    write('Apple').
 
 answer(web) :-
     write('Web').
@@ -284,23 +278,20 @@ answer(android) :-
 answer(frontEnd) :-
     write('Front-end').
 
-answer(backEnd) :-
-    write('Back-end').
-
-answer(startup) :-
-    write('Startup').
-
-answer(corporate) :-
-    write('Corporate').
-
 answer(iAmFan) :-
-    write('I\'m a fan!').
+    write('I am a fan!').
 
 answer(goodEnough) :-
-    write('Not Bad').
+    write('Good Enough').
 
 answer(suck) :-
-    write('Suck').
+    write('It Sucks').
+
+desc(csharp) :-
+    write('C#'), nl,
+    write('A popular choice for enterprise to create websites and Windows application using .NET framework'), nl,
+    write('Can be used to build websites with ASP.net a web Framework from Microsoft'), nl,
+    write('Similar to Java in basic syntax and some features').
 
 answer(yes) :-
     write('Yes').
@@ -315,7 +306,14 @@ answer(playDoh) :-
     write('Play-Doh').
 
 answer(oldUgly) :-
-    write('I\'ve an old & ugly toy, but I love it so much!').
+    write('I have an old and ugly toy and I like it so much').
+
+desc(java) :-
+    write('Java'), nl,
+    write('One of the most in demand & highest paying programming languages'), nl,
+    write('Slogan: write once, work everywhere') , nl ,
+    write('Very popular on all platforms because of its portability ') , nl.
+
 
 answer(easyWay) :-
     write('The easy way').
@@ -327,158 +325,155 @@ answer(harderWay) :-
     write('The slightly harder way').
 
 answer(hardestWay) :-
-    write('The really hard way (but easier to pick up other languages in the future)').
+    write('Extremely hard way (Suicide)').
 
 answer(auto) :-
-    write('Auto').
+    write('Automatic Transmission Car').
 
 answer(manual) :-
-    write('Manual').
+    write('Manual Transmission Car').
 
 
-% Language descriptions for the knowledge base
-describe(python) :-
+desc(python) :-
     write('Python'), nl,
     write('Widely regarded as the best programming language for beginners'), nl,
     write('Easiest to learn'), nl, 
     write('You can Build website using Django a popular framework for python') ,nl ,
     write('Widely Used in scientific,technical and academic Field such as Artificial Intelligence ') , nl.
 
-describe(java) :-
-    write('Java'), nl,
-    write('One of the most in demand & highest paying programming languages'), nl,
-    write('Slogan: write once, work everywhere') , nl ,
-    write('Very popular on all platforms because of its portability ') , nl.
+answer(google) :-
+    write('Google').
 
-describe(c) :-
+answer(microsoft) :-
+    write('Microsoft').
+
+answer(apple) :-
+    write('Apple').
+
+
+desc(c) :-
     write('C'), nl,
     write('Lingua franca of programming language'), nl,
     write('One of the oldest and most widely used language in the world'), nl ,
     write('Popular language for System and Hardware programming ') , nl.
 
-describe(cpp) :-
+desc(cpp) :-
     write('C++'), nl,
     write('Complex version of C with a lot more features'), nl,
     write('Widely used for developing games , industrial and performance critical applications'), nl,
     write('Recommended only if you have a mentor to guide you').
 
-describe(javascript) :-
+answer(backEnd) :-
+    write('Back-end').
+
+answer(startup) :-
+    write('Startup').
+
+answer(corporate) :-
+    write('Corporate').
+
+desc(javascript) :-
     write('JavaScript'), nl,
     write('Most popular clients-side web scripting language'), nl,
     write('One of the hottest programming language now, due to its increasing popularity as server side language(node.js)'), nl,
     write('A must learn for front-end web developer (HTML and CSS as well)').
 
-describe(csharp) :-
-    write('C#'), nl,
-    write('A popular choice for enterprise to create websites and Windows application using .NET framework'), nl,
-    write('Can be used to build websites with ASP.net a web Framework from Microsoft'), nl,
-    write('Similar to Java in basic syntax and some features').
-
-describe(ruby) :-
+desc(ruby) :-
     write('Ruby'), nl,
     write('Mostly known for its popular web framework, Ruby on Rails'), nl,
     write('Designed for fun and productive coding'), nl,
     write('Focuses on getting things done').
 
-describe(php) :-
-    write('PHP'), nl,
-    write('Suitable for building small and simple sites within a short time frame'), nl,
-    write('Supported by almost every web hosting services with lower price').
+what(objective) :-
+    write('Writing an objective'), nl,
+    where(goal).
+recommendation.
 
-describe(objectivec) :-
-    write('Objective-C'), nl,
-    write('Primary language used by Apple for MacOSX & iOS'), nl,
-    write('Choose this if you want to focus on developing iOS or OSX apps only').
-
-
-
-% Assigns an answer to questions from the knowledge base
 why(Answer) :-
     progress(why, Answer).
 why(Answer) :-
     \+ progress(why, _),
-    ask(why, Answer, [forChildren, donotKnow, earnMoney, forFun, forInterest, toImprove]).
+    askAQuestion(why, Answer, [forChildren, donotKnow, earnMoney, forFun, forInterest, toImprove]).
+
+desc(objectivec) :-
+    write('Objective-C'), nl,
+    write('Primary language used by Apple for MacOSX & iOS'), nl,
+    write('Choose this if you want to focus on developing iOS or OSX apps only').
 
 prefferedPlatform(Answer) :-
     progress(prefferedPlatform, Answer).
 prefferedPlatform(Answer) :-
     \+ progress(prefferedPlatform, _),
-    ask(prefferedPlatform, Answer, [doesNotMatter, gaming, mobile, facebook, google, microsoft, apple, web, enterprise]).
+    askAQuestion(prefferedPlatform, Answer, [doesNotMatter, gaming, mobile, facebook, google, microsoft, apple, web, enterprise]).
 
 preferredMobileOS(Answer) :-
     progress(preferredMobileOS, Answer).
 preferredMobileOS(Answer) :-
     \+ progress(preferredMobileOS, _),
-    ask(preferredMobileOS, Answer, [ios, android]).
+    askAQuestion(preferredMobileOS, Answer, [ios, android]).
 
 web(Answer) :-
     progress(web, Answer).
 web(Answer) :-
     \+ progress(web, _),
-    ask(web, Answer, [frontEnd, backEnd]).
+    askAQuestion(web, Answer, [frontEnd, backEnd]).
 
 workFor(Answer) :-
     progress(workFor, Answer).
 workFor(Answer) :-
     \+ progress(workFor, _),
-    ask(workFor, Answer, [startup, corporate]).
-
-microsoftOpinion(Answer) :-
-    progress(microsoftOpinion, Answer).
-microsoftOpinion(Answer) :-
-    \+ progress(microsoftOpinion, _),
-    ask(microsoftOpinion, Answer, [iAmFan, goodEnough, suck]).
-
-tryNew(Answer) :-
-    progress(tryNew, Answer).
-tryNew(Answer) :-
-    \+ progress(tryNew, _),
-    ask(tryNew, Answer, [yes, no]).
-
-favouriteToy(Answer) :-
-    progress(favouriteToy, Answer).
-favouriteToy(Answer) :-
-    \+ progress(favouriteToy, _),
-    ask(favouriteToy, Answer, [lego, playDoh, oldUgly]).
-
-learningPreference(Answer) :-
-    progress(learningPreference, Answer).
-learningPreference(Answer) :-
-    \+ progress(learningPreference, _),
-    ask(learningPreference, Answer, [easyWay, bestWay, harderWay, hardestWay]).
+    askAQuestion(workFor, Answer, [startup, corporate]).
 
 whichCar(Answer) :-
     progress(whichCar, Answer).
 whichCar(Answer) :-
     \+ progress(whichCar, _),
-    ask(whichCar, Answer, [auto, manual]).
+    askAQuestion(whichCar, Answer, [auto, manual]).
+
+microsoftOpinion(Answer) :-
+    progress(microsoftOpinion, Answer).
+microsoftOpinion(Answer) :-
+    \+ progress(microsoftOpinion, _),
+    askAQuestion(microsoftOpinion, Answer, [iAmFan, goodEnough, suck]).
+
+tryNew(Answer) :-
+    progress(tryNew, Answer).
+tryNew(Answer) :-
+    \+ progress(tryNew, _),
+    askAQuestion(tryNew, Answer, [yes, no]).
+
+favouriteToy(Answer) :-
+    progress(favouriteToy, Answer).
+favouriteToy(Answer) :-
+    \+ progress(favouriteToy, _),
+    askAQuestion(favouriteToy, Answer, [lego, playDoh, oldUgly]).
+
+desc(php) :-
+    write('PHP'), nl,
+    write('Suitable for building small and simple sites within a short time frame'), nl,
+    write('Supported by almost every web hosting services with lower price').
+
+learningPreference(Answer) :-
+    progress(learningPreference, Answer).
+learningPreference(Answer) :-
+    \+ progress(learningPreference, _),
+    askAQuestion(learningPreference, Answer, [easyWay, bestWay, harderWay, hardestWay]).
 
 
-% Outputs a nicely formatted list of answers
-% [First|Rest] is the Choices list, Index is the index of First in Choices
 answers([], _).
 answers([First|Rest], Index) :-
     write(Index), write(' -> '), answer(First), nl,
     NextIndex is Index + 1,
     answers(Rest, NextIndex).
 
+reset_answers :-
+      retract(progress(_, _)),
+        fail.
+    reset_answers.
 
-% Parses an Index and returns a Response representing the "Indexth" element in
-% Choices (the [First|Rest] list)
-parse(0, [First|_], First).
-parse(Index, [First|Rest], Response) :-
-    Index > 0,
-    NextIndex is Index - 1,
-    parse(NextIndex, Rest, Response).
-
-
-% Asks the Question to the user and saves the Answer
-ask(Question, Answer, Choices) :-
-    question(Question),
-    answers(Choices, 0),
-    read(Index),
-    write(''), nl,
-    parse(Index, Choices, Response),
-    asserta(progress(Question, Response)),
-    Response = Answer.
+main :-
+    welcome,
+    reset_answers,
+    findLanguage(Language),
+    desc(Language), nl.
 
